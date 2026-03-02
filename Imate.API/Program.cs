a477dd445d28229c451ea74cb8b03063a827e727
PhuDK <PhuDKHE186439@fpt.edu.vn>
2026-03-02 21:19:35 +0700

    giam .net xuong 8.0 thay openAPI bang swagger SwashBuckle

--- Imate.API/Program.cs
+++ Imate.API/Program.cs
@@ -2,7 +2,6 @@ using Imate.API.Configurations;
 using Imate.API.Business.Interfaces;
 using Imate.API.Business.Services;
 using Imate.API.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,14 +21,15 @@ builder.Services.AddTransient<GlobalExceptionMiddleware>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
