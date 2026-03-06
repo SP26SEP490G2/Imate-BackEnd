bb357668cf13f10577e1ae9e497eec5023d7c993
Cap1ainTNL <thuanpp11@gmail.com>
2026-03-06 21:41:04 +0700

    feature: Create APIs for add, update question for staff

---
+++ Imate.API/DataAccess/Interfaces/Classification/ICompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using Imate.API.Models.Entities;
+using Imate.API.Presentation.ResponseModels.Classification;
+
+namespace Imate.API.DataAccess.Interfaces.Classification
+{
+    public interface ICompanyRepository
+    {
+        Task<Company?> GetByIdAsync(int id);
+        Task AddAsync(Company company); 
+        Task UpdateAsync(Company company); 
+        Task<Company?> SetStatusAsync(int id, bool isActive);
+        Task<(IEnumerable<Company> Items, int TotalCount)> GetPagedListAsync(CompanyListRequestModel request);
+        Task<bool> NameExistsAsync(string name);
+        Task<bool> NameExistsExcludingIdAsync(string name, int excludeId);
+        IQueryable<Company> GetAllQueryable();
+    }
+}
---
+++ Imate.API/Presentation/ResponseModels/Classification/CompanyResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Imate.API.Presentation.ResponseModels.Classification
+{
+    public class CompanyResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
