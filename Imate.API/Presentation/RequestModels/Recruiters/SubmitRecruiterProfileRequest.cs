ee0e7fb1f0654787ae528bae38d9da14336cde91
TrucNGHE186655 <trucnghe186655@fpt.edu.vn>
2026-03-26 12:44:10 +0700

    feature: add company input for recruiter  api

--- Imate.API/Presentation/RequestModels/Recruiters/SubmitRecruiterProfileRequest.cs
+++ Imate.API/Presentation/RequestModels/Recruiters/SubmitRecruiterProfileRequest.cs
@@ -5,6 +5,7 @@ namespace Imate.API.Presentation.RequestModels.Recruiters
         public string CompanyName { get; set; } = string.Empty;
         public string? CompanyAddress { get; set; }
         public string? CompanyWebsite { get; set; }
+        public string? CompanyLogo { get; set; }
         public string Phone { get; set; } = string.Empty;
         
         // Optional original fields if still needed
