67053fdf928d8ccd5502080cf9d71c7748f66ef7
PhuDK <PhuDKHE186439@fpt.edu.vn>
2026-03-27 17:44:39 +0700

    refactor: change auditLog searchTerm

--- Imate.API/Presentation/ResponseModels/AuditLog/AuditLogListResponse.cs
+++ Imate.API/Presentation/ResponseModels/AuditLog/AuditLogListResponse.cs
@@ -6,7 +6,9 @@ namespace Imate.API.Presentation.ResponseModels.AuditLog
         public string StaffName { get; set; }
         public string StaffEmail { get; set; }
         public string Action { get; set; }
-        public string EntityType { get; set; }
+		public string? OldValue { get; set; }
+		public string? NewValue { get; set; }
+		public string EntityType { get; set; }
         public DateTime ActionTime { get; set; }
     }
 }
